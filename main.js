5f7cbc7cac8c0b029683e4ce574a59ac90c98af9
HaxagonusD <jquezad1@binghamton.edu>
2020-11-03 18:27:16 -0500

    I know this already

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 //Exercise #1
-// Define two arrays, evens and odds filled with even 
+// Define two arrays, evens and odds filled with even
 // and odd numbers
 // Using the spread operator, make a new
 // array with numbers that contains all of the evens and odds
 
-
+const odds = [3, 5, 7];
+const evens = [2, 4, 6];
+const nums1 = [...odds, ...evens];
 
 //Exercise #2
-// Define an object favoriteFoods that has keys representing three of 
+// Define an object favoriteFoods that has keys representing three of
 // your favorite foods and the values should be the restaurant where
 // they are from
-// Make another object and do the same thing but ask your 
-// imaginary friend what their favorite foods are. 
-// Use the spread operator to build a final allFoods with the 
+// Make another object and do the same thing but ask your
+// imaginary friend what their favorite foods are.
+// Use the spread operator to build a final allFoods with the
 // items from the two objects you just built
 
-
+faveFoods = { burgers: "Jacksons", pizza: "Difara", steak: "Benjamins" };
+friendFaveFoods = { sandwich: "subway", pasta: "carmines", salad: "chopped" };
+allFoods = { ...faveFoods, ...friendFaveFoods };
+console.log(allFoods);
 
 // Exercise #3
 // Define your favorite movie character in an object
@@ -29,4 +34,18 @@
 // 1) add a pet object to your person
 // 2) define three variables (age, breed, name) outside of the object
 // 3) add those variables to your pet
-// log the most important thing about it: the pet's name 
+// log the most important thing about it: the pet's name
+myself = { name: "Jonathan", age: "26", hometown: "Queens" };
+// const { name, age, hometown } = myself
+// console.log(name, age, hometown);
+const { name, age, ...me } = myself;
+console.log(name, age, me.hometown);
+const pet = {};
+myself = { ...myself, pet };
+let petName = "fred";
+let petAge = 1;
+let petBreed = "German-Shephard";
+myself.pet.Name = petName;
+myself.pet.Age = petAge;
+myself.pet.Breed = petBreed;
+console.log(myself.pet);
